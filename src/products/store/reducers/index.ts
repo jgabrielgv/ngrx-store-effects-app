3ccf059372d2a68463f60ed792789f297b8fefcf
= <jgmezvargas@gmail.com>
2018-06-13 16:04:11 -0600

    Reducers

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
 
 import * as fromPizzas from './pizzas.reducer';
 
@@ -13,3 +13,27 @@ export interface ProductsState {
 export const reducers: ActionReducerMap<ProductsState> = {
   pizzas: fromPizzas.reducer,
 };
+
+export const getProductsState = createFeatureSelector<ProductsState>(
+  'products'
+);
+
+// const state = {
+//   products: {
+//     pizzas: {
+//       data: [],
+//       loaded: false,
+//       loading: false
+//     }
+//   }
+// }
+
+// pizzas state
+export const getPizzaState = createSelector(
+  getProductsState, 
+  (state: ProductsState) => state.pizzas
+);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
